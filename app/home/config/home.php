ed38c4e82ede58ff95d88f6f2024edf8ab2e7be4
qiuyutao <1466406455@qq.com>
2019-10-23 17:07:20 +0800

    首页图片config

---
+++ app/home/config/home.php
@@ -0,0 +1,44 @@
+<?php
+//pro
+return [
+    'banner' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'supermarket' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'parttimejob' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'ticket' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'hotel' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'catering' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'school' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'rent_house' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'rent_car' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'secondhand' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+    'express' => 'http://qiniu.wanjunjiaoyu.com/erp/compress/20190918/temp1568776185.jpg',
+];
+/*
+return [
+    'apiKey'=>'EpI00UA-BXH5qWfgg4q5YJ5W5g5uTKEB',
+    'apiSecret'=>'7j6bo1pxNrIjKpzOySqC3S-rzfVk14rC',
+    'baseUri'=>'https://api-cn.faceplusplus.com/',
+    'timeout'=>30
+];
+//test
+return [
+    'apiKey'=>'Z1fZMFTe6Aqo8OGYTC2iaT4aCrhRaJOg',
+    'apiSecret'=>'skKJg2JSYK60UgF79vTo2wtPlbf3QtGs',
+    'baseUri'=>'https://api-cn.faceplusplus.com/',
+    'timeout'=>30
+];
+*/
+//pro-us
+return [
+    'apiKey'=>'cdQSZCKTyTh7mTEvv1Pi-gz7q6lSOJJ7',
+    'apiSecret'=>'HoFsxnPuiFYjDvlQNi-vAD-PsHddnHb0',
+    'baseUri' => 'https://api-us.faceplusplus.com/',
+    'timeout'=>30
+];
+//test-us
+return [
+    'apiKey' => '6TBf4sFQgNZE4JAz2bSZdwO6kvDlsXdd',
+    'apiSecret' => 'aZ20yuyjofVUek8U-8R5Xa3YGDBipvPr',
+    'baseUri' => 'https://api-us.faceplusplus.com/',
+    'timeout' => 30
+];
