fc6dfff940f9388aef2748435e20ff4bcf7d917f
qiuyutao <qiuyt@miaoqian.com>
2019-11-09 23:46:13 +0800

    add db

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* WARNING
+*
+* Manual changes to this file may cause a malfunction of the system.
+* Be careful when changing settings!
+*
+*/
+return array (
+    'adapter' => 'Mysql',
+    //'adapter' => 'Postgresql',
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'username' => 'shenghuogang',
+//    'password' => 'd245151981GM@',
+    'password' => '123456',
+    'dbname' => 'shenghuogang',
+    //'persistent' => false
+     'charset' => 'utf8'
+);
