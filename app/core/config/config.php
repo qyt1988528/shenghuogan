7d92226f684ff79002942527919b5e75a388d998
qiuyutao <qiuyt@miaoqian.com>
2019-11-11 23:46:26 +0800

    supermarket

--- app/core/config/config.php
+++ app/core/config/config.php
@@ -1,7 +1,34 @@
 <?php
 return [
-    'status' => [
-        'valid' => 1,
-        'invalid' => -1,
+    'data_status' => [
+        'valid' => 1,//有效
+        'invalid' => -1,//无效 指删除 这里删除只改状态并非物理删除
+    ],
+    'specs_unit' => [
+        //id=>unit_description
+        1 => 'kg',
+        2 => '瓶',
+        3 => '盒',
+        4 => '枚',
+        5 => '包',
+        6 => '袋',
+        7 => '只',
+        8 => '份',
+        9 => '桶',
+        10 => 'ML',
+        11 => 'L',
+    ],
+    'selling_status' => [
+        'selling' => 1,//在售
+        'unselling' => -1,//下架
+    ],
+    'is_recommend' => [
+        'recommend' => 1,//推荐
+        'normal' => -1,//正常
+    ],
+    'error_message' => [
+        'invalid_input' => 'Invalid input!',
+        'try_later' => 'Network Error. Please Try Again Later',
     ]
+
 ];
--- app/supermarket/controller/AdminController.php
+++ app/supermarket/controller/AdminController.php
@@ -9,30 +9,36 @@ use MDK\Controller;
  */
 class AdminController extends Controller
 {
-    public $insertFields = [
-        'title',
-        'img_url',
-        'original_price',
-        'self_price',
-        'description',
-        'specs',
-    ];
+    private $_error;
+
+    public function __construct()
+    {
+        $config = $this->app->core->config->config->toArray();
+        $this->_error = $config['error_message'];
+    }
 
     /**
-     * Index action.
+     * 创建
+     * Create action.
      * @return void
-     * @Route("/", methods="GET", name="supermarketadmin")
+     * @Route("/create", methods="POST", name="supermarketadmin")
      */
     public function createAction() {
+        //权限验证
         $postData = $this->request->getPost();
-        foreach ($this->insertFields as $v){
+        $insertFields = $this->app->supermarket->api->Helper()->getInsertFields();
+        foreach ($insertFields as $v){
             if(empty($postData[$v])){
-                $this->resultSet->error(1001,'Invalid input!');
+                $this->resultSet->error(1001,$this->_error['invalid_input']);
             }
         }
         try{
+            $insert = $this->app->supermarket->api->Helper()->createGoods($postData);
+            if(empty($insert)){
+                $this->resultSet->error(1002,$this->_error['try_later']);
+            }
             $data =[
-
+                'id' => $insert
             ];
         }catch (\Exception $e){
             $this->resultSet->error($e->getCode(),$e->getMessage());
@@ -41,14 +47,93 @@ class AdminController extends Controller
         $this->response->success($this->resultSet->toObject());
     }
 
+    /**
+     * 删除
+     * Create action.
+     * @return void
+     * @Route("/delete", methods="POST", name="supermarketadmin")
+     */
     public function deleteAction() {
+        //权限验证
+        $goodsId = $this->request->getPost('id');
+        if(empty($goodsId)){
+            $this->resultSet->error(1001,$this->_error['invalid_input']);
+        }
+        try{
+           $result = $this->app->supermarket->api->Helper()->deleteGoods($goodsId);
+           if($result){
+               $data = [
+                   'del_success' => $result
+               ];
+           }else{
+               $this->resultSet->error(1002,$this->_error['try_later']);
+           }
+        }catch (\Exception $e){
+            $this->resultSet->error($e->getCode(),$e->getMessage());
+        }
+        $this->resultSet->success()->setData($data);
+        $this->response->success($this->resultSet->toObject());
 
     }
+    /**
+     * 下架
+     * Create action.
+     * @return void
+     * @Route("/withdraw", methods="POST", name="supermarketadmin")
+     */
     public function withdrawAction() {
-
+        //权限验证
+        $goodsId = $this->request->getPost('id');
+        if(empty($goodsId)){
+            $this->resultSet->error(1001,$this->_error['invalid_input']);
+        }
+        try{
+           $result = $this->app->supermarket->api->Helper()->withdrawGoods($goodsId);
+           if($result){
+               $data = [
+                   'withdraw_success' => $result
+               ];
+           }else{
+               $this->resultSet->error(1002,$this->_error['try_later']);
+           }
+        }catch (\Exception $e){
+            $this->resultSet->error($e->getCode(),$e->getMessage());
+        }
+        $this->resultSet->success()->setData($data);
+        $this->response->success($this->resultSet->toObject());
     }
+    /**
+     * 更新
+     * Create action.
+     * @return void
+     * @Route("/update", methods="POST", name="supermarketadmin")
+     */
     public function updateAction() {
-
+        //权限验证
+        $postData = $this->request->getPost();
+        if(empty($postData['id'])){
+            $this->resultSet->error(1001,$this->_error['invalid_input']);
+        }
+        $updateFields = $this->app->supermarket->api->Helper()->getInsertFields();
+        foreach ($updateFields as $v){
+            if(empty($postData[$v])){
+                $this->resultSet->error(1001,$this->_error['invalid_input']);
+            }
+        }
+        try{
+            $result = $this->app->supermarket->api->Helper()->updateGoods($postData);
+            if($result){
+                $data = [
+                    'update_success' => $result
+                ];
+            }else{
+                $this->resultSet->error(1002,$this->_error['try_later']);
+            }
+        }catch (\Exception $e){
+            $this->resultSet->error($e->getCode(),$e->getMessage());
+        }
+        $this->resultSet->success()->setData($data);
+        $this->response->success($this->resultSet->toObject());
     }
 
 }
