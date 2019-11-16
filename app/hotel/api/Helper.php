a68f0497994e0cd6c4906afc315db17fec110ac2
qiuyutao <qiuyt@miaoqian.com>
2019-11-16 23:33:56 +0800

    hoteladmin

--- app/hotel/api/Helper.php
+++ app/hotel/api/Helper.php
@@ -15,15 +15,14 @@ class Helper extends Api
 
     public function getInsertFields(){
         return $insertFields = [
-            'title',
             'img_url',
-            'type_id',
+            'title',
+            'stock',//房间数
+            'cost_price',
             'original_price',
             'self_price',
+            'location',
             'description',
-            'specs',
-            'specs_unit_id',
-            'stock',
         ];
     }
     public function getDefaultInsertFields($postData){
@@ -39,7 +38,7 @@ class Helper extends Api
         //is_recommend、sort、update_time、status采用默认值
         return $defaultInsertFields;
     }
-    public function createGoods($postData){
+    public function createHotel($postData){
         try{
             $insertData = $this->getDefaultInsertFields($postData);
             foreach ($this->getInsertFields() as $v){
@@ -52,7 +51,7 @@ class Helper extends Api
             return 0;
         }
     }
-    public function updateGoods($postData){
+    public function updateHotel($postData){
         try{
             $updateData = ['id' => $postData['id']];
             $updateModel = $this->_model->findFirstById($postData['id']);
@@ -69,14 +68,14 @@ class Helper extends Api
         }
     }
     //下架
-    public function withdrawGoods($goodsId){
+    public function withdrawHotel($hotelId){
         try{
-            $updateModel = $this->_model->findFirstById($goodsId);
+            $updateModel = $this->_model->findFirstById($hotelId);
             if(empty($updateModel)){
                 return false;
             }
             $updateData = [
-                'id' => $goodsId,
+                'id' => $hotelId,
                 'is_selling' => $this->_config['selling_status']['unselling'],
             ];
             $updateModel->update($updateData);
@@ -85,15 +84,15 @@ class Helper extends Api
             return false;
         }
     }
-    public function deleteGoods($goodsId){
+    public function deleteHotel($hotelId){
         try{
             $invalid = $this->_config['data_status']['invalid'];
-            $updateModel = $this->_model->findFirstById($goodsId);
+            $updateModel = $this->_model->findFirstById($hotelId);
             if(empty($updateModel)){
                 return false;
             }
             $updateData = [
-                'id' => $goodsId,
+                'id' => $hotelId,
                 'status' => $invalid,
             ];
             $updateModel->update($updateData);
@@ -102,24 +101,15 @@ class Helper extends Api
             return false;
         }
     }
-    public function detail($goodsId){
-        $condition = "id = ".$goodsId;
+    public function detail($hotelId){
+        $condition = "id = ".$hotelId;
         $condition .= " and is_selling = ".$this->_config['selling_status']['selling'];
         $condition .= " and status = ".$this->_config['data_status']['valid'];
         $goods = $this->_model->findFirst($condition);
         return $goods;
     }
     public function search($goodsName){
-        /*
-        $slideAds = $this->modelsManager->createBuilder()
-            ->columns('cts.image_ratio,cts.image,cts.jump_url,cts.title,cts.ga_name,vc.condition')
-            ->from(['cts'=>'Supermarket\Model\SupermarketGoods'])
-            ->join('Core\Model\VersionControl','vc.table_id = cts.id and vc.table_name="cms_home_top_slide"','vc','LEFT')
-            ->where('is_selling = :selling: and status = :valid: and title like :goodsName:',['store'=>$store])
-            ->andWhere('b.store = :store: AND b.active=1 and platforms like :systemType:',['store'=>$store,'systemType' => '%'.$systemType.'%'])
-            ->orderBy('sort desc')
-            ->getQuery()
-            ->execute();*/
+        //暂时不支持房间搜索
         $goods = $this->modelsManager->createBuilder()
             ->columns('*')
             ->from(['sg'=>'Supermarket\Model\SupermarketGoods'])
